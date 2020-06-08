ab0109f1f0e5c832f4e0ea521d802e683be31056
hernandez87v <hernandez87v@hotmail.com>
2020-06-07 19:00:58 -0700

    Change to handleClick function

--- src/index.js
+++ src/index.js
@@ -52,12 +52,13 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         },
       ],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -71,6 +72,13 @@ class Game extends React.Component {
         },
       ]),
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0,
     });
   }
 
